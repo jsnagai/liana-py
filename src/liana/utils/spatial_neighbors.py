8c4b21905a7c2264d70462c9181acfd553438d51
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-10-24 13:42:33 +0200

    Fixed return type is np.ndarray and not np.array (function)

--- src/liana/utils/spatial_neighbors.py
+++ src/liana/utils/spatial_neighbors.py
@@ -38,7 +38,7 @@ def spatial_neighbors(adata: AnnData,
                       spatial_key: str = K.spatial_key,
                       key_added: str = K.spatial_key,
                       inplace: bool = V.inplace
-                      ) -> Union[np.array | None]:
+                      ) -> Union[np.ndarray | None]:
     """
     Generate spatial connectivity weights using Euclidean distance.
 
