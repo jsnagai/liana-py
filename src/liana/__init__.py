69858ddf9ba9947b25b862320ec659b1dc35bbe4
Nicolas Palacio <nicolaspalacio91@gmail.com>
2025-12-02 12:59:52 +0100

    Line wrapping in `__init__.py`

--- src/liana/__init__.py
+++ src/liana/__init__.py
@@ -1,12 +1,18 @@
 __version__ = '1.6.1'
 
-from liana import method as mt, plotting as pl, resource as rs, multi as mu, utils as ut, testing
+from liana import method as mt
+from liana import plotting as pl
+from liana import resource as rs
+from liana import multi as mu
+from liana import utils as ut
+from liana import testing
 
 # done after everything has been imported (adapted from scanpy)
 import sys
 from scanpy._utils import annotate_doc_types
 
-sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['mt', 'pl', 'rs', 'ut']})
+sys.modules.update({f'{__name__}.{m}': globals()[m]
+                    for m in ['mt', 'pl', 'rs', 'ut']})
 annotate_doc_types(sys.modules[__name__], 'liana')
 
 del sys, annotate_doc_types
